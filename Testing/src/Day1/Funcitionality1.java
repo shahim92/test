8ab9ef62647b72cedc023b4ab3f18b688d389259
Mohammad Ahmad <49246417+shahim92@users.noreply.github.com>
2020-04-27 06:23:54 -0400

    Add files via upload

---
+++ Testing/src/Day1/Funcitionality1.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class Funcitionality1 {
+	public static void main(String[]args) {
+		int time = 20;
+		if (time<18) {
+			System.out.println("Good Day");
+		}
+		else {
+			System.out.println("Good evening");
+			
+		}
+		
+	}
+	public static void fe() {
+		if(20>18) {
+			System.out.println("20 is greater than 18");
+		}
+	}
+
+}
